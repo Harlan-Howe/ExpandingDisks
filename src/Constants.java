9424dd40139a9b70209267908c112e1987dc7cdf
Harlan Howe <harlan.howe@kinkaid.org>
2024-12-17 22:52:16 -0600

    Added MAX_DISTANCE_TO_REMOVE_SPIKE

--- src/Constants.java
+++ src/Constants.java
@@ -4,5 +4,6 @@ public interface Constants
     public final int SCREEN_HEIGHT = 800;
 
     public final int MILLISECONDS_PER_STEP = 10;
+    public final int MAX_DISTANCE_TO_REMOVE_SPIKE = 75;
 
 }
