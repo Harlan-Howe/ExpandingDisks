06beb012302ba7d07c8bc3f4848200b5edfee03b
Harlan Howe <harlan.howe@kinkaid.org>
2024-12-17 21:15:04 -0600

    Initial Commit

---
+++ src/Constants.java
@@ -0,0 +1,6 @@
+public interface Constants
+{
+    public final int SCREEN_WIDTH = 800;
+    public final int SCREEN_HEIGHT = 800;
+
+}
---
+++ src/ExpandingDisksRunner.java
@@ -0,0 +1,7 @@
+public class ExpandingDisksRunner
+{
+    public void main()
+    {
+
+    }
+}
