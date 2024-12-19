9602dc1d2ad0102f99b6f2e8315bab0c006b30bb
Harlan Howe <harlan.howe@kinkaid.org>
2024-12-18 18:17:51 -0600

    added NUM_TRIES_TO_MAKE_NEW_DISK

--- src/Constants.java
+++ src/Constants.java
@@ -5,5 +5,6 @@ public interface Constants
 
     public final int MILLISECONDS_PER_STEP = 10;
     public final int MAX_DISTANCE_TO_REMOVE_SPIKE = 75;
+    public final int NUM_TRIES_TO_MAKE_NEW_DISK = 20;
 
 }
