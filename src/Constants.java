0b80a568f15f637b1904c81863f73072739e0293
Harlan Howe <harlan.howe@kinkaid.org>
2025-01-15 22:55:18 -0600

    Documented Constants; added background color.

--- src/Constants.java
+++ src/Constants.java
@@ -1,10 +1,19 @@
+import java.awt.*;
+
 public interface Constants
 {
+    // the size of the ExpandingDisksPanel. The window will be taller to accommodate the title and the buttons row.
     public final int SCREEN_WIDTH = 800;
     public final int SCREEN_HEIGHT = 800;
 
-    public final int MILLISECONDS_PER_STEP = 10;
-    public final int MAX_DISTANCE_TO_REMOVE_SPIKE = 75;
-    public final int NUM_TRIES_TO_MAKE_NEW_DISK = 20;
+    public final Color BACKGROUND_COLOR = Color.GRAY;
+
+    public final int MILLISECONDS_PER_STEP = 10;  // controls the speed at which the disks appear to expand.
+
+    public final int MAX_DISTANCE_TO_REMOVE_SPIKE = 75; // how close does the user need to shift-click to a spike to
+    //                                                      "count" as selecting it?
+
+    public final int NUM_TRIES_TO_MAKE_NEW_DISK = 20; // how many times in a row should the computer try to make a new
+    //                                                      disk before giving up and deciding the screen is too full?
 
 }
